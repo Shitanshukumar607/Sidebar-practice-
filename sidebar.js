6b4550b35da437de4021b11044314ac3c3102fc9
Shitanshukumar607 <147914822+Shitanshukumar607@users.noreply.github.com>
2023-12-04 19:42:03 +0530

    Add files via upload

---
+++ sidebar.js
@@ -0,0 +1,11 @@
+let Menu = document.getElementById("menu")
+let Back = document.getElementById("back")
+let Sidebar = document.getElementById("container")
+
+function sidebarshow() {
+    Sidebar.style.visibility = "visible";
+}
+
+function sidebarhide() {
+    Sidebar.style.visibility = "hidden";
+}
